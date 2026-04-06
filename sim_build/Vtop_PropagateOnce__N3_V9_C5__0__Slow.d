04add146b32e47a5e6e5f7b1c3ba9260c0e58d45
Michael Geurtjens <geurtjens@me.com>
2026-04-06 20:17:21 +1000

    ignore sim_build directory

--- sim_build/Vtop_PropagateOnce__N3_V9_C5__0__Slow.d
+++
@@ -1,2 +0,0 @@
-Vtop_PropagateOnce__N3_V9_C5__0__Slow.o: \
- Vtop_PropagateOnce__N3_V9_C5__0__Slow.cpp Vtop__pch.h
--- sim_build/Vtop_PropagateOnce__N3_V9_C5__1.d
+++
@@ -1,2 +0,0 @@
-Vtop_PropagateOnce__N3_V9_C5__1.o: Vtop_PropagateOnce__N3_V9_C5__1.cpp \
- Vtop__pch.h
